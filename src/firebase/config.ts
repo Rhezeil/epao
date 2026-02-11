97fc63d681445a1c308796ea4703a5c724e6a809
Glei Rhezeil <rhezeilglei@gmail.com>
2026-02-11 04:10:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9420005325-9026e",
+  "appId": "1:448633568084:web:7c697a40874c6b43ada6c1",
+  "apiKey": "AIzaSyAv19hvwLFKF8NnJh5opYwK2fJ8ZXGcN5o",
+  "authDomain": "studio-9420005325-9026e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "448633568084"
+};
